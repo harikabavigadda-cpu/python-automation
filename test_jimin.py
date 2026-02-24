3bbeab0bfc6777ac14bb1c9dc09a1fcc377cef99
Harika <harikabavigadda@gmail.com>
2026-02-24 15:22:41 +0530

    Added changes to test_jimin and tets_jk

--- test_jimin.py
+++ test_jimin.py
@@ -1,7 +1,8 @@
 import json
 def test_jimin():
-    data={"name":"jimin"}
+    data={"name":"jimin","age":"34"}
     with open("jimin.json","w")as file:
         json.dump(data,file,indent=4)
     assert data ["name"]=="jimin"
+    assert data ["age"]=="34"
 
--- tets_jk.py
+++ tets_jk.py
@@ -1,6 +1,8 @@
 import json
 def test_jk():
-    data={"village":"seoul"}
+    data={"village":"seoul","name":"jk"}
     with open("jk.json","w")as file:
         json.dump(data,file,indent=4)
     assert data["village"]=="seoul"
+    assert data["name"]=="jk"
+print(test_jimin())
