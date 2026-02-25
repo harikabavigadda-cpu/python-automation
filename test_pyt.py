4535e8debea6d329f0855d6fbae61e1c84710ea2
Harika <harikabavigadda@gmail.com>
2026-02-25 13:28:40 +0530

    Updated test file

--- test_pyt.py
+++ test_pyt.py
@@ -1,10 +1,11 @@
 import json
 def test_py():
-    data = {"name" : "heyyy","k" : "jk","age" : 34}
+    data = {"name" : "heyyy","k" : "jk","age" : 34,"city":"hyd"}
     with open("jk.json", "w")as file:
         json.dump(data, file, indent=4)
     assert data ["name"]=="heyyy"
     assert data ["k"]=="jk"
+    assert data ["age"]==34
 
 
 
