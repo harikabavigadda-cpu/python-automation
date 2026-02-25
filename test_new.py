11c213196d8fe1ac7756d08749f495ad9ac6e2e8
Harika <harikabavigadda@gmail.com>
2026-02-25 16:02:59 +0530

    changes modify

--- test_new.py
+++ test_new.py
@@ -1,8 +1,9 @@
 import json
 def test_json():
-    data = {"bts": "ot", "blackpink": "jin"}
+    data = {"bts": "ot", "blackpink": "jin","country":"seoul"}
     with open("kim.json", "w") as file:
         json.dump(data, file, indent=4)
     assert data ["bts"] == "ot"
     assert data ["blackpink"] == "jin"
+    assert data ["country"] == "seoul"
 
