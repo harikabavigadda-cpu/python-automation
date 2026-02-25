dcf9d0106b7c76ca222f5cb2d768f7cdd47fad1e
Harika <harikabavigadda@gmail.com>
2026-02-25 12:03:28 +0530

    Saving test_pyt.py before pull

--- test_pyt.py
+++ test_pyt.py
@@ -1,9 +1,10 @@
 import json
 def test_py():
-    data = {"name" : "heyyy","k" : "jk"}
+    data = {"name" : "heyyy","k" : "jk","age" : 34}
     with open("jk.json", "w")as file:
         json.dump(data, file, indent=4)
     assert data ["name"]=="heyyy"
+    assert data ["k"]=="jk"
 
 
 
