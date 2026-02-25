df4754ea1a6bf974e4a1c4f843600c399a5d4b82
Harika <harikabavigadda@gmail.com>
2026-02-25 12:00:17 +0530

    Updated test files

--- tets_jk.py
+++ tets_jk.py
@@ -1,11 +1,12 @@
 import json
 def test_jk():
-    data={"village":"seoul","name":"jk","age":25,"gender":"male"}
+    data={"village":"seoul","name":"jk","age":25,"gender":"male","section":"A"}
     with open("jk.json","w")as file:
         json.dump(data,file,indent=4)
     assert data["village"]=="seoul"
     assert data["name"]=="jk"
     assert data["age"]==25
     assert data["gender"]=="male"
+    assert data["section"]=="A"
 print(test_jimin())
 
